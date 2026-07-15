01f92fb567a6fe5d1e45f194bc8c532addcb4e35
14870-source <14870@holbertonstudents.com>
2026-07-15 19:16:58 +0000

    Complete task 1

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -4,20 +4,32 @@ int main(void)
 {
     int choice;
 
-    printf("Simple Calculator\n");
-    printf("1) Add\n");
-    printf("2) Subtract\n");
-    printf("3) Multiply\n");
-    printf("4) Divide\n");
-    printf("0) Quit\n");
-    printf("Choice: ");
-
-    scanf("%d", &choice);
-
-    if (choice == 0)
+    do
     {
-        printf("Bye!\n");
-    }
+        printf("Simple Calculator\n");
+        printf("1) Add\n");
+        printf("2) Subtract\n");
+        printf("3) Multiply\n");
+        printf("4) Divide\n");
+        printf("0) Quit\n");
+        printf("Choice: ");
+
+        scanf("%d", &choice);
+
+        if (choice >= 1 && choice <= 4)
+        {
+            /* سيتم إضافة العمليات في المهام القادمة */
+        }
+        else if (choice == 0)
+        {
+            printf("Bye!\n");
+        }
+        else
+        {
+            printf("Invalid choice\n");
+        }
+
+    } while (choice != 0);
 
     return (0);
 }
