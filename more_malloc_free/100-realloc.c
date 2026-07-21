b466b5bc731b6b709ac0356e0d74e0518f855be0
14870-source <14870@holbertonstudents.com>
2026-07-21 11:17:18 +0000

    Add _realloc function

---
+++ more_malloc_free/100-realloc.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: pointer to previous memory
+ * @old_size: size of old memory
+ * @new_size: size of new memory
+ *
+ * Return: pointer to new memory, or NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr;
+	char *old_ptr;
+	unsigned int i;
+	unsigned int min_size;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (old_size < new_size)
+		min_size = old_size;
+	else
+		min_size = new_size;
+
+	for (i = 0; i < min_size; i++)
+		new_ptr[i] = old_ptr[i];
+
+	free(ptr);
+
+	return (new_ptr);
+}
