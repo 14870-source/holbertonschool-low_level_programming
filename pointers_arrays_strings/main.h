5df95b2b4408aec4030c653daed3754053fe74a1
14870-source <14870@holbertonstudents.com>
2026-07-22 14:38:56 +0000

    Define struct dog

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,5 +24,19 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _isdigit(char *s);
+int _strlen(char *s);
+void print_error(void);
+int *multiply(char *num1, char *num2, int len1, int len2);
 
 #endif
