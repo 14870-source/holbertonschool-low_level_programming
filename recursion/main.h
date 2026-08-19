7c28a87d0c729316b7555b7d1e6f73b8b009bcab
14870-source <14870@holbertonstudents.com>
2026-08-19 18:43:53 +0000

    Add recursive prime number checker

--- recursion/main.h
+++ recursion/main.h
@@ -8,5 +8,7 @@ int _putchar(char c);
 int factorial(int n);
 int _sqrt_recursion(int n);
 int sqrt_helper(int n, int guess);
+int is_prime_number(int n);
+int prime_helper(int n, int divisor);
 
 #endif
